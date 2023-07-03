d5e5eb3a8dffc9cd276c13deb4ebeda74f811639
Erfan Azizzadeh <erfanazpc@gmail.com>
2023-07-03 17:42:30 +0430

    pack update one

--- src/pack/cycle.java
+++ src/pack/cycle.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pack;
+import java.util.Scanner;
 
-/**
- *
- * @author Asus
- */
-public class cycle {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Circle {
+  private  static  int radious;
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+      //get number
+        System.out.println("Circle :  \n Enter your number : ");
+        radious = scanner.nextInt();
+        //print
+        System.out.println("Circle{"+ " perimerter: "+ getPerimeter() + ", Area: "+ getArea()+", Diameter: "+getDiameter());
+
+
+
     }
-    
+        public static int getPerimeter(){
+            return (int) (getDiameter()*Math.PI); }
+        public static int getDiameter(){
+        return radious*2;
+        }
+        public static int getArea(){
+        return (int) (radious*radious * Math.PI);// 3.14 cast the number to int
+        }
+
+
 }
